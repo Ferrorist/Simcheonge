f17a207601d94d1df26c160ba56ca0034ac7ef35
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-07 17:29:12 +0900

    Add: 백엔드 프로젝트 폴더 추가

---
+++ simcheonge_server/src/test/java/com/e102/simcheonge_server/SimcheongeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e102.simcheonge_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimcheongeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
