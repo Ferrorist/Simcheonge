627f60537dc2a7e650cd6881115689bca4e6729a
jius <tjqmone@naver.com>
2024-03-14 15:10:02 +0900

    Feat : 마이페이지-북마크구현

---
+++ simcheonge_front/lib/screens/bookmark_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class BookmarkScreen extends StatefulWidget {
+  const BookmarkScreen({super.key});
+
+  @override
+  _BookmarkScreenState createState() => _BookmarkScreenState();
+}
+
+class _BookmarkScreenState extends State<BookmarkScreen> {
+  List<String> bookmarkedItems =
+      List<String>.generate(20, (i) => 'Item ${i + 1}'); // 더미 데이터 20개 생성
+  String searchQuery = ''; // 검색 쿼리를 저장하는 문자열
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('정책 검색'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () async {
+              showSearch(
+                context: context,
+                delegate: DataSearch(bookmarkedItems),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: bookmarkedItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(bookmarkedItems[index]),
+            trailing: IconButton(
+              icon: const Icon(Icons.bookmark),
+              onPressed: () {
+                setState(() {
+                  bookmarkedItems.removeAt(index);
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final List<String> items;
+
+  DataSearch(this.items);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          print('끔');
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        print('끔');
+
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? items
+        : items.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        title: Text(suggestionList[index]),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
