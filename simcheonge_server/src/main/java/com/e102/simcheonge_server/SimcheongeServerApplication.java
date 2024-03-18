f42d8c48a1126dc24594b5030584a1d3ee44efca
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-03-18 17:58:48 +0900

    Test : 자동화 배포 실험

---
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/SimcheongeServerApplication.java
@@ -0,0 +1,13 @@
+package com.e102.simcheonge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimcheongeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimcheongeServerApplication.class, args);
+	}
+
+}
