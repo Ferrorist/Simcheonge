a035695265188ae45e24e24759f8179e67e41e8e
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-03-21 14:10:39 +0900

    Add : develop 브랜치 배포 테스트용 파일 추가

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/common/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.e102.simcheonge_server.common;
-
-import com.e102.simcheonge_server.common.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
