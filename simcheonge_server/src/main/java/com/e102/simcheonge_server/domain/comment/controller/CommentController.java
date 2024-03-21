c177774fd6bf98584afe83f246bdfb86b2194195
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-21 11:30:58 +0900

    Fix: 댓글 조회 시 시간 제거

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/comment/controller/CommentController.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/comment/controller/CommentController.java
@@ -1,10 +1,13 @@
 package com.e102.simcheonge_server.domain.comment.controller;
 
-import com.e102.simcheonge_server.domain.comment.dto.CommentRequest;
+import com.e102.simcheonge_server.common.util.ResponseUtil;
+import com.e102.simcheonge_server.domain.comment.dto.request.CommentCreateRequest;
+import com.e102.simcheonge_server.domain.comment.dto.request.CommentReadRequest;
 import com.e102.simcheonge_server.domain.comment.service.CommentService;
 import com.e102.simcheonge_server.domain.user.dto.SessionUser;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +19,16 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<?> addComment(@RequestBody CommentRequest commentRequest, @SessionAttribute(name = "user", required = false)
-    SessionUser loginUser){
+    public ResponseEntity<?> addComment(@RequestBody CommentCreateRequest commentRequest, @SessionAttribute(name = "user", required = false)
+    SessionUser loginUser) {
         commentService.addComment(commentRequest, loginUser.getUserId());
-        return ResponseEntity.ok("댓글 등록에 성공했습니다.");
+        return ResponseUtil.buildBasicResponse(HttpStatus.OK, "댓글 등록에 성공했습니다.");
+    }
+
+    @GetMapping("/{commentType}/{referencedId}")
+    public ResponseEntity<?> getBoardComments(@PathVariable("commentType") String commentType, @PathVariable("referencedId") int referencedId, @SessionAttribute(name = "user", required = false)
+    SessionUser loginUser) {
+        return ResponseUtil.buildBasicResponse(HttpStatus.OK, commentService.getBoardComments(commentType, referencedId, loginUser.getUserId()));
     }
 
 }
