e281af635359974b13b005c267bd3b8c245dcdbb
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-21 09:14:59 +0900

    Feat: 게시판 카테고리 데이터 추가

---
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/policy/controller/PolicyController.java
@@ -0,0 +1,14 @@
+package com.e102.simcheonge_server.domain.policy.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("/policy")
+public class PolicyController {
+
+}
