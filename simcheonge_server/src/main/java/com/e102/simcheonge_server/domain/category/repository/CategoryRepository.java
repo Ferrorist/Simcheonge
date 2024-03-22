699c5a14a98cf7977a496a68d873db5c88b7763e
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-22 16:19:14 +0900

    Feat: 정책 카테고리 조회

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category/repository/CategoryRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category/repository/CategoryRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,String> {
-    Optional<Category> findByCode(String code);
-
-    List<Category> findAll();
+//    Optional<Category> findByCode(String code);
+//
+    List<Category> findAllByCodeNot(String code);
 }
--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category_detail/repository/CategoryDetailRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category_detail/repository/CategoryDetailRepository.java
@@ -5,6 +5,10 @@ import com.e102.simcheonge_server.domain.category_detail.entity.CategoryDetailId
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryDetailRepository extends JpaRepository<CategoryDetail, CategoryDetailId> {
+
+    List<CategoryDetail> findAllByCode(String code);
 }
