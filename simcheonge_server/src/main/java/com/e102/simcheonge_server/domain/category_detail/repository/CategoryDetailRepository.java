b8f140e1890a79535518e967b98d62f48cf10a88
김민진 <tjmg726@naver.com>
2024-03-25 14:45:55 +0900

    Feat: 게시판 임시 커밋

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category_detail/repository/CategoryDetailRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/category_detail/repository/CategoryDetailRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryDetailRepository extends JpaRepository<CategoryDetail, CategoryDetailId> {
 
     List<CategoryDetail> findAllByCode(String code);
+    Optional<CategoryDetail> findByCodeAndNumber(String code, int number);
 }
--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/post/repository/PostRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/post/repository/PostRepository.java
@@ -14,17 +14,27 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     // 특정 게시글 상세 조회
     Optional<Post> findByPostId(int postId);
 
-    // 특정 키워드가 포함된 게시글 검색 (제목 또는 내용)
-    @Query("SELECT p FROM Post p WHERE p.postName LIKE %:keyword% OR p.postContent LIKE %:keyword%")
-    List<Post> findByKeyword(@Param("keyword") String keyword);
 
-    // 특정 사용자가 작성한 게시글 조회
-    List<Post> findByUserId(int userId);
 
-    @Query("SELECT p FROM Post p WHERE (:keyword IS NULL OR (p.postName LIKE %:keyword% OR p.postContent LIKE %:keyword%)) AND (:categoryCode IS NULL OR p.categoryDetail.code = :categoryCode) AND (:categoryNumber IS NULL OR p.categoryDetail.number = :categoryNumber)")
-    List<Post> findByKeywordAndCategoryCodeAndCategoryNumber(@Param("keyword") String keyword, @Param("categoryCode") String categoryCode, @Param("categoryNumber") Integer categoryNumber);
 
-    // 카테고리 코드와 번호로 게시글 검색
-    @Query("SELECT p FROM Post p WHERE p.categoryDetail.code = :categoryCode AND p.categoryDetail.number = :categoryNumber")
-    List<Post> findByCategoryCodeAndCategoryNumber(@Param("categoryCode") String categoryCode, @Param("categoryNumber") Integer categoryNumber);
+//    // 특정 키워드가 포함된 게시글 검색 (제목 또는 내용)
+//    @Query("SELECT p FROM Post p WHERE p.postName LIKE %:keyword% OR p.postContent LIKE %:keyword%")
+//    List<Post> findByKeyword(@Param("keyword") String keyword);
+//
+//    // 특정 사용자가 작성한 게시글 조회
+//    List<Post> findByUserId(int userId);
+//
+//    @Query("SELECT p FROM Post p WHERE (:keyword IS NULL OR (p.postName LIKE %:keyword% OR p.postContent LIKE %:keyword%)) AND (:categoryCode IS NULL OR p.categoryCode = :categoryCode) AND (:categoryNumber IS NULL OR p.categoryNumber = :categoryNumber)")
+//    List<Post> findByKeywordAndCategoryCodeAndCategoryNumber(@Param("keyword") String keyword, @Param("categoryCode") String categoryCode, @Param("categoryNumber") Integer categoryNumber);
+//
+//    // 사용자 ID와 카테고리 코드, 카테고리 넘버에 따라 게시글을 찾는 쿼리 메서드 추가
+//    List<Post> findByUserIdAndCategoryCodeAndCategoryNumber(int userId, String categoryCode, int categoryNumber);
+//
+//    // 특정 카테고리와 키워드로 게시글 검색
+//    @Query("SELECT p FROM Post p WHERE p.categoryCode = :categoryCode AND p.categoryNumber = :categoryNumber AND " +
+//            "(p.postName LIKE %:keyword% OR p.postContent LIKE %:keyword%)")
+//    List<Post> findByCategoryCodeAndCategoryNumberAndKeyword(@Param("categoryCode") String categoryCode, @Param("categoryNumber") Integer categoryNumber, @Param("keyword") String keyword);
+//
+//    // 특정 카테고리의 모든 게시글 조회
+//    List<Post> findByCategoryCodeAndCategoryNumber(String categoryCode, Integer categoryNumber);
 }
