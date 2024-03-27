b5834ec66953c93d25fa05d3f69045b0ba6259bc
jius <tjqmone@naver.com>
2024-03-27 17:55:45 +0900

    Feat : 로그인 및 회원가입 구현, 로그아웃 구현 중

--- simcheonge_front/lib/screens/signup_screen.dart
+++ simcheonge_front/lib/screens/signup_screen.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:developer' as developer;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simcheonge_front/main.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
 
   @override
-  _SignUpScreenState createState() => _SignUpScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
@@ -27,7 +27,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
       return;
     }
 
-    final url = Uri.parse('https://j10e102.p.ssafy.io/api/user/signup');
+    final url = Uri.parse('https://j10e102.p.ssafy.io/api/users/signup');
     final requestData = jsonEncode({
       'userNickname': _nicknameController.text,
       'userLoginId': _idController.text,
@@ -43,13 +43,19 @@ class _SignUpScreenState extends State<SignUpScreen> {
       );
 
       if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('회원가입에 성공했습니다.')),
+        // 회원가입 성공 시 로그인 상태 저장 및 홈 화면으로 이동
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setBool('isLoggedIn', true);
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => const MyHomePage()), // 가정: 로그인 성공 후 이동할 화면
         );
-        Navigator.pop(context);
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('회원가입에 실패했습니다: ${response.body}')),
+          SnackBar(
+              content: Text('회원가입에 실패했습니다: ${json.decode(response.body)}')),
         );
       }
     } catch (e) {
@@ -59,56 +65,41 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
   }
 
-// 닉네임 중복 확인 함수
   Future<void> checkNickname() async {
     final url = Uri.parse(
-        'https://j10e102.p.ssafy.io/api/user/check-nickname/${_nicknameController.text}');
+        'https://j10e102.p.ssafy.io/api/users/check-nickname/${_nicknameController.text}');
     try {
       final response = await http.get(url);
-
       if (response.statusCode == 200) {
-        // 중복이 없는 경우
         ScaffoldMessenger.of(context)
             .showSnackBar(const SnackBar(content: Text("사용 가능한 닉네임입니다.")));
       } else {
-        // 중복인 경우 또는 그 외 에러
         ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text("이미 사용중인 닉네임이거나 조회에 실패했습니다.")));
       }
     } catch (e) {
-      // HTTP 요청 중 예외 발생 시
       developer.log('닉네임 중복 검사 중 오류 발생', error: e.toString());
-      // 또는 print 함수를 사용할 수도 있습니다.
-      print('닉네임 중복 검사 중 오류 발생: ${e.toString()}');
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text("닉네임 중복 검사 중 오류가 발생했습니다: $e")));
     }
   }
 
-  // ID 중복 확인 함수
   Future<void> checkLoginId() async {
     final url = Uri.parse(
-        'https://j10e102.p.ssafy.io/api/user/check-loginId/${_idController.text}');
+        'https://j10e102.p.ssafy.io/api/users/check-loginId/${_idController.text}');
     try {
       final response = await http.get(url);
-
       if (response.statusCode == 200) {
-        // 중복이 없는 경우
         ScaffoldMessenger.of(context)
             .showSnackBar(const SnackBar(content: Text("사용 가능한 ID입니다.")));
       } else {
-        // 중복인 경우 또는 그 외 에러
         ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text("이미 사용중인 ID이거나 조회에 실패했습니다.")));
       }
     } catch (e) {
-      // HTTP 요청 중 예외 발생 시
       developer.log('ID 중복 검사 중 오류 발생', error: e.toString());
-      // 또는 print 함수를 사용할 수도 있습니다.
-      print('ID 중복 검사 중 오류 발생: ${e.toString()}');
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content:
-              Text("ID 중복 검사 중 오류가 발생했습니다: ${e.toString()}"))); // 예외 메시지 출력
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("ID 중복 검사 중 오류가 발생했습니다: ${e.toString()}")));
     }
   }
 
@@ -123,44 +114,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _nicknameController,
-                    decoration: const InputDecoration(
-                      labelText: '닉네임',
-                      hintText: '2~8자 한글/영문/한글+숫자/영문+숫자/한글+영문',
-                    ),
-                  ),
-                ),
-                // 중복 확인 버튼
-                ElevatedButton(
-                  onPressed: checkNickname,
-                  child: const Text('중복 확인'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _idController,
-                    decoration: const InputDecoration(
-                      labelText: 'ID',
-                      hintText: '6~10자 영문/영문+숫자',
-                    ),
-                  ),
-                ),
-                // 중복확인 버튼
-                ElevatedButton(
-                  onPressed: checkLoginId,
-                  child: const Text('중복 확인'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
+            buildTextField(_nicknameController, '닉네임',
+                '2~8자 한글/영문/한글+숫자/영문+숫자/한글+영문', checkNickname),
+            buildTextField(_idController, 'ID', '6~10자 영문/영문+숫자', checkLoginId),
             TextField(
               controller: _passwordController,
               decoration: const InputDecoration(
@@ -186,6 +142,27 @@ class _SignUpScreenState extends State<SignUpScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildTextField(TextEditingController controller, String label,
+      String hint, Function() onTap) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: label,
+              hintText: hint,
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: onTap,
+          child: const Text('중복 확인'),
+        ),
+      ],
     );
   }
 
--- simcheonge_front/lib/widgets/side_app_bar.dart
+++ simcheonge_front/lib/widgets/side_app_bar.dart
@@ -5,6 +5,7 @@ import 'package:simcheonge_front/screens/bookmark_post_screen.dart';
 import 'package:simcheonge_front/screens/my_policy_comment_screen.dart';
 import 'package:simcheonge_front/screens/my_post_comment_screen.dart';
 import 'package:simcheonge_front/screens/my_post_screen.dart';
+import 'package:simcheonge_front/services/auth_service.dart';
 
 class SideAppBar extends StatelessWidget {
   final Function(int) changePage; // 페이지 변경 함수를 위한 변수 추가
@@ -242,6 +243,7 @@ class SideAppBar extends StatelessWidget {
                 title: const Text('로그아웃'),
                 onTap: () {
                   // 로그아웃 기능 구현
+                  logout(context);
                   print('로그아웃');
                 },
               ),
