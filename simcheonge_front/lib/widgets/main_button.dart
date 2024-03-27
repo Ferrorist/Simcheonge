f08af256f819b5995f44b8fa501c8369be74ba4e
SeoJunHa96 <139415975+SeoJunHa96@users.noreply.github.com>
2024-03-27 11:23:33 +0900

    Feat: 검색 API 연동 및 메인 화면 수정

--- simcheonge_front/lib/widgets/main_button.dart
+++ simcheonge_front/lib/widgets/main_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainButton extends StatelessWidget {
-  final String name, descrip; // 'sub' 제거
+  final String name; // 'sub' 제거
   final IconData icon;
   final bool isInverted;
   final _blackColor = const Color(0xFF1F2123);
@@ -11,7 +11,6 @@ class MainButton extends StatelessWidget {
   const MainButton({
     super.key,
     required this.name,
-    required this.descrip,
     required this.icon,
     required this.isInverted,
     this.onPressed,
@@ -41,29 +40,18 @@ class MainButton extends StatelessWidget {
           children: [
             // 이름 텍스트: 좌측 상단에 위치
             Positioned(
-              top: 8,
-              left: 20,
+              top: 12,
+              left: 23,
               child: Text(
                 name,
                 style: TextStyle(
                   color: isInverted ? _blackColor : Colors.white,
-                  fontSize: 33, // 텍스트 크기 조정
+                  fontSize: 32, // 텍스트 크기 조정
                   fontWeight: FontWeight.w600,
                 ),
               ),
             ),
-            Positioned(
-              top: 60,
-              left: 45,
-              child: Text(
-                descrip,
-                style: TextStyle(
-                  color: isInverted ? _blackColor : Colors.white,
-                  fontSize: 18, // 텍스트 크기 조정
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
+
             // 아이콘: 우측 하단에 위치, 일부 잘린 상태로 표시
             Positioned(
               right: -4, // 아이콘을 우측으로 이동시켜 잘리게 함
