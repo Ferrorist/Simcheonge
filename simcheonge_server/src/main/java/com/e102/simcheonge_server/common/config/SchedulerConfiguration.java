ebf7488edb5c1c7743926117b21dd5b1d4cfebee
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-28 09:21:39 +0900

    Fix: 미가공 정책 데이터 스케줄러 시간 변경

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/common/config/SchedulerConfiguration.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/common/config/SchedulerConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class SchedulerConfiguration {
     private final YouthPlcyService youthPlcyService;
 
-    @Scheduled(cron="0 3 4 ? * *")
+    @Scheduled(cron="0 24 9 ? * *") //초 분 시 일 월 요일: 매일 4시 0분 0초에 실행
     public void run(){
         youthPlcyService.insertUnprocessedPolicyData();
     }
