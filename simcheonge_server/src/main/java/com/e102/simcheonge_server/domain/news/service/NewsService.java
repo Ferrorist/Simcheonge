a605af19698c0641585b9fdc07bc4b5c3aae647f
Jonggil-dev <jonggil.jeong24@gmail.com>
2024-03-31 05:18:14 +0900

    Feat : 뉴스 조회 구현 중

---
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/news/service/NewsService.java
@@ -0,0 +1,111 @@
+package com.e102.simcheonge_server.domain.news.service;
+
+import com.e102.simcheonge_server.domain.news.dto.NewsItem;
+import com.e102.simcheonge_server.domain.news.dto.NewsListResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class NewsService {
+    @Value("${NAVER_CLIENT_ID}")
+    private String clientId;
+    @Value("${NAVER_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Value("${NAVER_API_URL}")
+    private String naverApiUrl;
+
+
+    public List<NewsItem> getNewsList() {
+
+        String text = "청년 정책";
+        try {
+            text = URLEncoder.encode(text, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("검색어 인코딩 실패",e);
+        }
+
+        String apiURL = naverApiUrl + text;    // JSON 결과
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        String responseBody = get(apiURL,requestHeaders);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        NewsListResponse newsListResponse;
+        try {
+            newsListResponse = objectMapper.readValue(responseBody, NewsListResponse.class);
+        } catch (IOException e) {
+            throw new RuntimeException("뉴스 데이터 파싱 실패", e);
+        }
+
+        // 여기서 반환되는 값 확인 (콘솔에서 확인)
+        System.out.println(responseBody);
+        return newsListResponse.getItems();
+    }
+
+    private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 오류 발생
+                return readBody(con.getInputStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+
+    private static String readBody(InputStream body) {
+        // InputStreamReader 생성 시 인코딩을 UTF-8로 명시
+        InputStreamReader streamReader = new InputStreamReader(body, StandardCharsets.UTF_8);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+        }
+    }
+}
+
