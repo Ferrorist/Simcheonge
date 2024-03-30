1c4e3efbbd2eb51f39ab6523ed1f3892c937ef72
김민진 <tjmg726@naver.com>
2024-03-30 23:17:10 +0900

    Feat: 북마크 등록 기능 구현

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/bookmark/dto/request/BookmarkRequest.java
+++
@@ -1,9 +0,0 @@
-package com.e102.simcheonge_server.domain.bookmark.dto.request;
-
-public class BookmarkRequest {
-    private int userId;
-    private int referenceId;
-    private String bookmarkType;
-
-    // Getters and Setters
-}
--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/bookmark/repository/BookmarkRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/bookmark/repository/BookmarkRepository.java
@@ -2,6 +2,8 @@ package com.e102.simcheonge_server.domain.bookmark.repository;
 
 import com.e102.simcheonge_server.domain.bookmark.entity.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
 }
