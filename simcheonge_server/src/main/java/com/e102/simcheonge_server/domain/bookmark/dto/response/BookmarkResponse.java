40b9271b3e372c3d090204a74814e12e90c2a293
김민진 <tjmg726@naver.com>
2024-03-31 03:35:33 +0900

    Refactor: 북마크 기능 완성

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/bookmark/dto/response/BookmarkResponse.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/bookmark/dto/response/BookmarkResponse.java
@@ -1,47 +1,15 @@
 package com.e102.simcheonge_server.domain.bookmark.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookmarkResponse {
     private int bookmarkId;
     private int userId;
     private int referencedId;
     private String bookmarkType;
-
-    public BookmarkResponse(int bookmarkId, int userId, int referencedId, String bookmarkType) {
-        this.bookmarkId = bookmarkId;
-        this.userId = userId;
-        this.referencedId = referencedId;
-        this.bookmarkType = bookmarkType;
-    }
-
-    public int getBookmarkId() {
-        return bookmarkId;
-    }
-
-    public void setBookmarkId(int bookmarkId) {
-        this.bookmarkId = bookmarkId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public int getReferencedId() {
-        return referencedId;
-    }
-
-    public void setReferencedId(int referencedId) {
-        this.referencedId = referencedId;
-    }
-
-    public String getBookmarkType() {
-        return bookmarkType;
-    }
-
-    public void setBookmarkType(String bookmarkType) {
-        this.bookmarkType = bookmarkType;
-    }
 }
