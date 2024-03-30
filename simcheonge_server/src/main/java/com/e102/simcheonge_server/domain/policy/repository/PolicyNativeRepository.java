fcf5a77a327bf37bc612efdf21ccaa60fe4614e1
object1997428 <71408968+object1997428@users.noreply.github.com>
2024-03-31 00:34:23 +0900

    Fix: 검색 필터 sql 수정

--- simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/policy/repository/PolicyNativeRepository.java
+++ simcheonge_server/src/main/java/com/e102/simcheonge_server/domain/policy/repository/PolicyNativeRepository.java
@@ -23,7 +23,7 @@ public class PolicyNativeRepository {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public PageImpl<Object[]> searchPolicy(String keyword, List<CategoryDetailSearchRequest> detailList, Date startDate, Date endDate, Pageable pageable) {
+    public PageImpl<Object[]> searchPolicy(String keyword, List<CategoryDetailSearchRequest> detailList, Date startDate, Date endDate, List<String> codeList, Pageable pageable) {
 
         log.info("Repository keyword={}", keyword);
         String subQuery = "";
@@ -39,9 +39,14 @@ public class PolicyNativeRepository {
             }
             subQuery += ") detailList ON pcd.category_code = detailList.code AND " +
                     "pcd.category_number = detailList.number " +
-                    "GROUP BY pcd.policy_id " +
-                    "HAVING COUNT(pcd.policy_id) = " + detailList.size() + " " +
-                    ")";
+                    "GROUP BY pcd.policy_id ";
+            if (!codeList.isEmpty()) {
+                subQuery += " HAVING SUM(CASE WHEN pcd.category_code = '"+codeList.get(0)+"' THEN 1 ELSE 0 END) >= 1";
+            }
+            for (int i = 1; i < codeList.size(); i++) {
+                subQuery += " AND SUM(CASE WHEN pcd.category_code = '"+codeList.get(i)+"' THEN 1 ELSE 0 END) >= 1";
+            }
+            subQuery+=" )";
         }
 
         boolean containsDetail = detailList.stream()
@@ -52,24 +57,24 @@ public class PolicyNativeRepository {
         if (!keyword.isEmpty()) {
             sqlQuery += "and (policy_name like '%" + keyword + "%' " +
                     "or policy_support_content like '%" + keyword + "%' ) ";
-            countQuery+="and (policy_name like '%" + keyword + "%' " +
+            countQuery += "and (policy_name like '%" + keyword + "%' " +
                     "or policy_support_content like '%" + keyword + "%' ) ";
         }
         if (containsDetail) {
             Date currentDate = new Date();
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            sqlQuery +="and (policy_start_date >= '"+sdf.format(startDate)+"' and policy_end_date <= '"+sdf.format(endDate)+"' )";
-            countQuery+="and (policy_start_date >= '"+sdf.format(startDate)+"' and policy_end_date <= '"+sdf.format(endDate)+"' )";
+            sqlQuery += "and (policy_start_date >= '" + sdf.format(startDate) + "' and policy_end_date <= '" + sdf.format(endDate) + "' )";
+            countQuery += "and (policy_start_date >= '" + sdf.format(startDate) + "' and policy_end_date <= '" + sdf.format(endDate) + "' )";
         }
         sqlQuery += subQuery;
-        countQuery+=subQuery;
-
+        countQuery += subQuery;
+        log.info("sqlQuery={}", sqlQuery);
         Query cquery = entityManager.createNativeQuery(countQuery);
         Long count = (Long) cquery.getSingleResult();
 
         sqlQuery += " LIMIT " + pageable.getPageSize() + " OFFSET " + pageable.getOffset();
         Query query = entityManager.createNativeQuery(sqlQuery);
-        log.info("sqlQuery={}", sqlQuery);
+
 
         @SuppressWarnings("unchecked")
         List<Object[]> resultList = query.getResultList();
