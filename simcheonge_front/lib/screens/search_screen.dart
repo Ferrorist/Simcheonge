855c11db476b9b4e9b0cf3a8e7fd60954640ca2e
SeoJunHa96 <139415975+SeoJunHa96@users.noreply.github.com>
2024-03-30 10:22:54 +0900

    Feat: 필터 기능 구현중
    초기화 버튼, 페이지 네이션, 필터 초기화, 앱 종료시 초기화, stt 등 구현완료

--- simcheonge_front/lib/screens/search_screen.dart
+++ simcheonge_front/lib/screens/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
@@ -5,6 +6,8 @@ import 'package:simcheonge_front/screens/search_filter.dart';
 import 'dart:ui';
 import 'package:simcheonge_front/models/search_model.dart';
 import 'package:simcheonge_front/services/search_api.dart';
+import 'package:simcheonge_front/models/filter_model.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -20,13 +23,46 @@ class _SearchScreenState extends State<SearchScreen> {
   int _currentPage = 0;
   bool _isFetching = false;
   bool _hasMore = true; // 더 불러올 데이터가 있는지
+  final List<CategoryList> _selectedFilters = [];
+  final int _totalPages = 0;
+
+  late SpeechToText _speech;
 
   @override
   void initState() {
     super.initState();
+    _speech = SpeechToText();
+    _initializeSpeech();
+    _loadSelectedFilters();
     _controller.addListener(_searchListener);
     _scrollController.addListener(_scrollListener);
+
     _fetchData(); // 초기 데이터 로드
+  }
+
+  Future<void> _loadSelectedFilters() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? selectedFiltersStr = prefs.getString('selectedFilters');
+    if (selectedFiltersStr != null) {
+      final List<dynamic> selectedFiltersJson = jsonDecode(selectedFiltersStr);
+      final List<CategoryList> loadedFilters =
+          selectedFiltersJson.map((e) => CategoryList.fromJson(e)).toList();
+      setState(() {
+        _selectedFilters.clear();
+        _selectedFilters.addAll(loadedFilters);
+      });
+    }
+  }
+
+  void _initializeSpeech() async {
+    bool isAvailable = await _speech.initialize();
+    if (isAvailable) {
+      // 음성 입력이 사용 가능한 경우
+      print('Speech recognition initialized successfully.');
+    } else {
+      // 음성 입력이 사용 불가능한 경우
+      print('Error initializing speech recognition.');
+    }
   }
 
   @override
@@ -36,63 +72,104 @@ class _SearchScreenState extends State<SearchScreen> {
     super.dispose();
   }
 
-  void _scrollListener() {
-    if (_scrollController.position.pixels ==
-            _scrollController.position.maxScrollExtent &&
-        _hasMore) {
-      _fetchData();
-    }
-  }
-
   void _searchListener() {
+    _loadSelectedFilters(); // 필터 정보를 업데이트합니다.
     _filteredPolices.clear();
     _currentPage = 0;
     _hasMore = true;
     _fetchData();
   }
 
-  Future<void> _fetchData() async {
+  void _openFilterScreen() async {
+    final returnedFilters = await Navigator.push<List<CategoryList>>(
+      context,
+      MaterialPageRoute(builder: (context) => const FilterScreen()),
+    );
+
+    if (returnedFilters != null) {
+      _updateFilters(returnedFilters);
+      _searchListener();
+    }
+  }
+
+  Future<void> saveSelectedFilters(List<CategoryList> selectedFilters) async {
+    final prefs = await SharedPreferences.getInstance();
+    // 선택된 필터들을 JSON 문자열로 변환
+    String selectedFiltersJson =
+        jsonEncode(selectedFilters.map((filter) => filter.toJson()).toList());
+    // 'selectedFilters' 키를 사용하여 저장
+    await prefs.setString('selectedFilters', selectedFiltersJson);
+    // 저장된 데이터 로그로 확인
+    print("Saved filters: $selectedFiltersJson");
+  }
+
+  Future<List<CategoryList>> loadSelectedFilters() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? selectedFiltersJson = prefs.getString('selectedFilters');
+    if (selectedFiltersJson != null) {
+      List<dynamic> decodedFilters = jsonDecode(selectedFiltersJson);
+      return decodedFilters
+          .map((filterJson) => CategoryList.fromJson(filterJson))
+          .toList();
+    }
+    return [];
+  }
+
+  void _fetchData() async {
     if (_isFetching || !_hasMore) return;
 
     _isFetching = true;
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-    final prefs = await SharedPreferences.getInstance();
-    // SharedPreferences에서 필터 목록과 날짜 범위를 불러옵니다.
+    // SharedPreferences에서 최신 필터 정보 불러오기
     final String filtersString = prefs.getString('filters') ?? '[]';
-    List<Map<String, dynamic>> filters =
-        List<Map<String, dynamic>>.from(json.decode(filtersString));
+    List<dynamic> filtersJson = jsonDecode(filtersString);
+    List<Map<String, dynamic>> filters = filtersJson.map((filter) {
+      return {"code": filter['code'], "number": filter['number']};
+    }).toList();
 
     final String startDate = prefs.getString('startDate') ?? '';
     final String endDate = prefs.getString('endDate') ?? '';
 
     try {
       final SearchModel response = await SearchApi().searchPolicies(
-        _controller.text, // 검색어
-        filters, // 필터 목록
-        startDate: startDate, // 시작 날짜 (이름 있는 인자로 전달)
-        endDate: endDate, // 종료 날짜 (이름 있는 인자로 전달)
+        {
+          "keyword": _controller.text,
+          "list": filters,
+          if (startDate.isNotEmpty) "startDate": startDate,
+          if (endDate.isNotEmpty) "endDate": endDate,
+        },
+        _currentPage, // 페이지 번호 추가
       );
 
-      // API 응답에서 List<Content>를 올바르게 추출
-      final List<Content> newData = response.data?.content ?? [];
-
       setState(() {
-        if (newData.isEmpty) {
+        if (response.data?.content?.isEmpty ?? true) {
           _hasMore = false;
         } else {
-          _currentPage++;
-          _filteredPolices.addAll(newData); // 올바른 타입의 데이터 추가
+          _currentPage++; // 다음 페이지 번호로 증가합니다.
+          _filteredPolices
+              .addAll(response.data!.content!); // 현재 페이지의 데이터를 추가합니다.
         }
       });
     } catch (e) {
-      print(e); // 에러 처리
+      print("Error fetching data: $e");
     } finally {
       _isFetching = false;
     }
   }
 
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        _hasMore) {
+      // 추가 검색 결과를 가져올 수 있는지 여부를 확인합니다.
+      _fetchData(); // 다음 페이지의 데이터를 불러옵니다.
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool hasFilters = _selectedFilters.isNotEmpty;
     return Scaffold(
       appBar: AppBar(
         title: const Text('검색',
@@ -112,7 +189,7 @@ class _SearchScreenState extends State<SearchScreen> {
                 prefixIcon: IconButton(
                   icon: const Icon(Icons.mic),
                   onPressed: () {
-                    // 음성 검색 기능 구현 필요
+                    _startListening();
                   },
                 ),
                 suffixIcon: IconButton(
@@ -124,21 +201,52 @@ class _SearchScreenState extends State<SearchScreen> {
               ),
             ),
           ),
+          Visibility(
+            visible: _selectedFilters.isNotEmpty,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Wrap(
+                spacing: 6.0,
+                children: _selectedFilters
+                    .map((filter) => Chip(
+                          label: Text(filter.name ?? ''),
+                          onDeleted: () {
+                            _removeFilter(filter);
+                          },
+                        ))
+                    .toList(),
+              ),
+            ),
+          ),
           Padding(
-            padding: const EdgeInsets.only(left: 15.0),
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(_createRoute());
-                  },
+                  onTap: _openFilterScreen, // 필터 화면을 여는 메서드 호출
                   child: const Row(
                     children: [
                       Icon(Icons.filter_alt, color: Colors.blue),
                       SizedBox(width: 8),
                       Text('필터',
                           style: TextStyle(fontSize: 16, color: Colors.blue)),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: _clearAll,
+                  child: const Row(
+                    children: [
+                      Icon(
+                        Icons.restart_alt_outlined,
+                        color: Colors.red,
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text('전체해제',
+                          style: TextStyle(fontSize: 16, color: Colors.red)),
                     ],
                   ),
                 ),
@@ -181,24 +289,101 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) =>
-          const FilterScreen(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(-1.0, 0.0);
-        const end = Offset.zero;
-        const curve = Curves.ease;
+  void _startListening() async {
+    if (!_speech.isAvailable) {
+      print('음성 입력 사용 불가능');
+      return;
+    }
 
-        var tween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-        var offsetAnimation = animation.drive(tween);
-
-        return SlideTransition(
-          position: offsetAnimation,
-          child: child,
+    if (!_speech.isListening) {
+      try {
+        await _speech.listen(
+          onResult: (result) {
+            setState(() {
+              _controller.text = result.recognizedWords;
+            });
+          },
         );
-      },
-    );
+      } catch (e) {
+        print('음성 입력 시작 중 오류 발생: $e');
+      }
+    } else {
+      print('이미 음성 입력 중입니다.');
+    }
+  }
+
+  void _removeFilter(CategoryList filter) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    // 'filters' 목록에서 해당 필터 제거
+    List<dynamic> filters = jsonDecode(prefs.getString('filters') ?? '[]');
+    // 필터를 제대로 식별하기 위해 'code'와 'number' 모두 확인
+    filters.removeWhere((item) =>
+        item['code'] == filter.code &&
+        item['number'].toString() == filter.number);
+    await prefs.setString('filters', jsonEncode(filters));
+
+    // 'selectedFilters' 목록에서 해당 필터 제거
+    List<dynamic> selectedFilters =
+        jsonDecode(prefs.getString('selectedFilters') ?? '[]');
+    selectedFilters.removeWhere((item) =>
+        item['code'] == filter.code &&
+        item['number'].toString() == filter.number);
+    await prefs.setString('selectedFilters', jsonEncode(selectedFilters));
+
+    // UI 업데이트
+    setState(() {
+      _selectedFilters.removeWhere((selectedFilter) =>
+          selectedFilter.code == filter.code &&
+          selectedFilter.number == filter.number);
+    });
+  }
+
+  void _clearAll() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('filters');
+    await prefs.remove('startDate');
+    await prefs.remove('endDate');
+    await prefs.remove('selectedFilters'); // 선택된 필터 목록도 초기화합니다.
+
+    setState(() {
+      _controller.text = '';
+      _filteredPolices.clear();
+      _selectedFilters.clear(); // 화면에서 선택된 필터 목록을 초기화합니다.
+      _currentPage = 0;
+      _hasMore = true;
+    });
+
+    await saveSelectedFilters(_selectedFilters); // 변경된 필터를 저장합니다.
+    _searchListener(); // 모든 필터를 해제한 후 검색 결과를 초기화합니다.
+  }
+
+  void _updateFilters(List<CategoryList> returnedFilters) {
+    setState(() {
+      _selectedFilters.clear();
+      _selectedFilters.addAll(returnedFilters);
+    });
+    Route createRoute() {
+      return PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            const FilterScreen(),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(-1.0, 0.0);
+          const end = Offset.zero;
+          const curve = Curves.ease;
+
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          var offsetAnimation = animation.drive(tween);
+
+          return SlideTransition(
+            position: offsetAnimation,
+            child: child,
+          );
+        },
+      );
+    }
+
+    _searchListener();
   }
 }
--- simcheonge_front/lib/services/search_api.dart
+++ simcheonge_front/lib/services/search_api.dart
@@ -5,30 +5,20 @@ import 'package:flutter/material.dart'; // debugPrint를 사용하기 위해 추
 
 class SearchApi {
   Future<SearchModel> searchPolicies(
-      String keyword, List<Map<String, dynamic>> filters,
-      {String? startDate, String? endDate}) async {
-    final url = Uri.parse('https://j10e102.p.ssafy.io/api/policy/search');
+      Map<String, dynamic> requestBody, int page) async {
+    final url = Uri.parse(
+        'https://j10e102.p.ssafy.io/api/policy/search?page=$page'); // 페이지 번호 추가
 
-    // 요청 본문을 구성합니다.
-    var requestBody = jsonEncode({
-      "keyword": keyword,
-      "list": filters,
-      "startDate": startDate,
-      "endDate": endDate,
-    });
-
-    // 디버그 출력을 사용하여 요청 본문을 출력합니다.
-    debugPrint('Request body: $requestBody');
+    debugPrint('Request body: ${jsonEncode(requestBody)}');
 
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
-      body: requestBody,
+      body: jsonEncode(requestBody),
     );
 
     if (response.statusCode == 200) {
-      final responseData =
-          json.decode(utf8.decode(response.bodyBytes, allowMalformed: true));
+      final responseData = json.decode(utf8.decode(response.bodyBytes));
       return SearchModel.fromJson(responseData);
     } else {
       throw Exception('Failed to load policies');
